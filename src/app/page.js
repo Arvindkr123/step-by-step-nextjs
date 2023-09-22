4336827f3b615fe3061f9c603d982ba7f3645a2c
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-22 15:14:37 +0530

    knowledge about the structure

--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,10 @@ import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const [name, setName] = useState("Arvind");
-  const apple = (item) => {
-    // alert(item);
-    setName("Thanks");
-  };
-
-  function InnerComponent() {
-    return <h1>Inner Component</h1>;
-  }
+  
   return (
     <main className={styles.main}>
-      <h1>Events functions and States in NEXT JS {name}</h1>
-      {/* <InnerComponent/> */}
-      {InnerComponent()}
-      <button className={styles.btn} onClick={() => apple()}>
-        Click Me
-      </button>
+
     </main>
   );
 }
