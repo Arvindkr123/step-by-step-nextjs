fcd20c09567972ebb41f1d28a808013e403a3794
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-23 12:51:00 +0530

    addd catch all segment files

---
+++ src/app/study/[...lecture]/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+export default function Lecture({ params }) {
+  //console.log(params);
+  const { lecture } = params;
+  return (
+    <div>
+      lecture here of your collges
+      <ul>
+        {lecture.map((lec) => {
+          return <li>{lec}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/study/page.jsx
@@ -0,0 +1,7 @@
+export default function Study() {
+  return (
+    <div>
+      <h1>all lecture of College</h1>
+    </div>
+  );
+}
