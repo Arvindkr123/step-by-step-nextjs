2cf38559a14155d88b7bed63fbdd0671dca1de8f
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-23 20:06:39 +0530

    starting styling in next js

--- src/app/productList/Button.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-
-const Button = ({ price, children }) => {
-  return <button style={{padding:"5px", margin:"5px"}} onClick={() => alert(price)}>{children}</button>;
-};
-
-export default Button;
