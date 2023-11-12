bf53897ab16c61ed382627e6f04c15f91e8afc2f
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-11-12 16:09:02 +0530

    change

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles from "./about.module.css";
+import Image from "next/image";
 
 const About = () => {
   return (
-    <div>
-      This is About Page
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src={
+            "https://www.pexels.com/photo/photo-of-people-using-laptops-3194521/"
+          }
+          fill={true}
+          alt="meeting image"
+          className={styles.img}
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>
+            Handcrafting award winning digital experiences
+          </h2>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
